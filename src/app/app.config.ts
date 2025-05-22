262f5e71588011bd324fddc7504221465994e35a
AkshK <harikrushna.satani.1005@tenzed.com>
2025-05-22 17:17:50 +0530

    Add a new 'echarts' library.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,17 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { NgxEchartsModule } from 'ngx-echarts';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimationsAsync(),
     provideClientHydration(),
+    importProvidersFrom(
+      NgxEchartsModule.forRoot({echarts: () => import('echarts')})
+    )
   ]
 };
